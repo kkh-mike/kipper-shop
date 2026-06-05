ce6d2504f08a4664ca2febfbe5d50dccc46ab1cf
Mike <kkh199205@gmail.com>
2026-06-06 00:38:04 +0800

    Create product list and detail page

--- server.js
+++ server.js
@@ -7,6 +7,25 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+app.get('/products', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'products.html'));
+});
+
+app.get('/products/:id', (req, res) => {
+
+  const productId = req.params.id;
+
+  res.send(`
+      <h1>Product Detail</h1>
+      <h2>Product ID : ${productId}</h2>
+
+      <a href="/products">
+        Back To Products
+      </a>
+  `);
+
+});
+
 app.listen(3000, () => {
   console.log('Kipper Shop running...');
 });
